80a889c54913a2d86497b1b97995de7ff07e595c
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-12-31 19:46:55 -0300

    Projeto salvo

--- Shooting Nave/Assets/Script/Player.cs
+++ Shooting Nave/Assets/Script/Player.cs
@@ -5,12 +5,12 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("Aperte enter");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Oi");
     }
 }
