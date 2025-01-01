2341523ad188dd112e052d551d448d6967a6419e
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-12-31 21:38:46 -0300

    Criadas primeiras funções de movimento
    Funções que movimentam o objeto, aumentar e diminuir a velocidade.

--- Shooting Nave/Assets/Script/Player.cs
+++ Shooting Nave/Assets/Script/Player.cs
@@ -2,21 +2,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float velocidade;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Aperte enter");
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Oi");
+        movimento();
+        velocidadeAumentada();
+        velocidadeDiminuida();
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    //Função responsável pelo movimento do objeto.
+    void movimento()
+    {
+        float x = Input.GetAxis("Horizontal"); 
+        float y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.W) || y == 1)
         {
-            transform.Translate(0, 5 * Time.deltaTime, 0);
+            transform.Translate(0, velocidade * Time.deltaTime, 0);
         }
+        if (Input.GetKey(KeyCode.S) || y == -1)
+        {
+            transform.Translate(0, -velocidade * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || x == 1)
+        {
+            transform.Translate(velocidade * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || x == -1)
+        {
+            transform.Translate(-velocidade * Time.deltaTime, 0, 0);
+        }
+    }
 
+    //Aumenta a velocidade do objeto 
+    void velocidadeAumentada() 
+    {
+        float A = Input.GetAxis("Fire1");
+
+        if(Input.GetKey(KeyCode.Q) || A == 1)
+        {
+            velocidade++;
+        }
+    }
+
+    //Diminui a velocidade do objeto
+    void velocidadeDiminuida() 
+    {
+        float B = Input.GetAxis("Fire2");
+
+        if (Input.GetKey(KeyCode.R) || B == 1)
+        {
+            velocidade--;
+        }
     }
 }
