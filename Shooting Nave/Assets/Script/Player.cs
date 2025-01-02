994862d063780029a1d52a798badd9fb938ebfc1
Joao-victor-da-silva <75917980+Joao-victor-da-silva@users.noreply.github.com>
2025-01-02 19:26:14 -0300

    função exemplo manipular propriedade

--- Shooting Nave/Assets/Script/Player.cs
+++ Shooting Nave/Assets/Script/Player.cs
@@ -7,12 +7,13 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-  
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //exemplo();
         movimento();
         velocidadeAumentada();
         velocidadeDiminuida();
@@ -61,6 +62,32 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.R) || B == 1)
         {
             velocidade--;
+        }
+    }
+
+    void exemplo()
+    {
+        //chegar ate uma propriedade
+        print(gameObject.transform.position.x); //<-exceção  
+        print(GetComponent<Transform>().position.x);//<-sempre
+        print(this.GetComponent<Transform>().position.x);//<-mais antiga
+
+        //quando solta a tecla P
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
+        }
+
+        //quando mantem precionado a tecla P
+        if (Input.GetKey(KeyCode.P))
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
+        }
+
+        //quando precionou a tecla P
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
         }
     }
 }
