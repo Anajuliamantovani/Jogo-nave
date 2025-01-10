d563ec8bb82feb7c1de1525eabb05df38fe018b7
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2025-01-10 00:00:27 -0300

    LPOO
    Criada a função Fatorial e Impar Par do desafio, entendido com sucesso o FOR

--- Shooting Nave/Assets/Script/LPOO.cs
+++ Shooting Nave/Assets/Script/LPOO.cs
@@ -1,18 +1,22 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
 
 public class LPOO : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print(Soma(10, 10));
+        /*print(Soma(10, 10));
         print(Subtracao(15, 5));
         print(Divisao(10,2));
         print(Multiplicacao(5,2));
         print("média" + Media(10, 10, 10));
         ImprimirCrescente(1, 10);
         ImprimirDecrescente(10,2);
-        tabuada(5);
+        tabuada(5);*/
+        fatorial(20);
+        ImparPar(true);
     }
 
     // Update is called once per frame
@@ -73,4 +77,47 @@ public class LPOO : MonoBehaviour
         return 0;
     }
 
+    int fatorial(int num1)
+    {
+        int resultado = 1;
+        int num2 = 0;
+        string imprimir;
+
+        imprimir = (num1 + "!" + "=");
+        for ( int i = 1; i < num1 ; i++)
+        {
+            resultado = resultado * (i + 1);
+            imprimir = imprimir + (i + "x");
+            num2 = i;
+        }
+        imprimir = imprimir + ((num2 + 1) + "=");
+        print(imprimir + resultado);
+        return 0;
+    }
+
+    //Uma função chamda ImparPar que recebe  parametro do tipo booleano se for true, ele imprime do 0 ate 10 
+    //apenas os numeros imperes e se o parametro for false, imprimir apenas os pares.
+    void ImparPar(bool num1)
+    {
+        if (num1 == false)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    print(i);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    print(i);
+                }
+            }
+        }
+    }
 }
