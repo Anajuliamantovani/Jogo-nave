cda9016ffb2a15e65ca56f663bd4bf39e144c42f
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2025-02-20 22:41:56 -0300

    Criado uma little interface

--- Shooting Nave/Assets/Script/Exemplo.cs
+++ Shooting Nave/Assets/Script/Exemplo.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 
-public class Exemplo
-{
-    public string Nome;
-    public string Endereço;
-    public int Telefone;
-    public int idade;
-    public bool estadoCivil;
-    public char Genero;
-}
 public class Example : MonoBehaviour
 {
-    public string Nome;
-    public string Endereço;
-    public int Telefone;
-    public int idade;
-    public bool estadoCivil;
-    public char Genero;
+    public TMP_Text caixaTexto;
+    public Button OK;
+    public TMP_InputField EnviarTexto;
+    public TMP_InputField EnviarTexto2;
+
+    private void Start()
+    {
+        caixaTexto.text = "O rato roeu a roupa do rei de roma";
+    }
+
+    public void TrocaMensagem() 
+    {
+        
+        int num1 = int.Parse(EnviarTexto.text);
+        int num2 = int.Parse(EnviarTexto2.text);
+
+        caixaTexto.text = "OLA MUNDO" + (num1 + num2).ToString();
+    }
 }
 
