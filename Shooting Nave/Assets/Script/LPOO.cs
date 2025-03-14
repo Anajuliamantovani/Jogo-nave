5dae8d36f9f9cc59bb5375bb454c9b2ba782ba54
Joao-victor-da-silva <75917980+Joao-victor-da-silva@users.noreply.github.com>
2025-03-13 22:21:46 -0300

    5 DESAFIOS

--- Shooting Nave/Assets/Script/LPOO.cs
+++ Shooting Nave/Assets/Script/LPOO.cs
@@ -1,269 +1,29 @@
-using NUnit.Framework.Constraints;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
-using System;
-using static Unity.Burst.Intrinsics.X86;
 
 public class LPOO : MonoBehaviour
 {
-    public int idade, idadeMinima;
-    public float n1, n2, n3;
-    public float peso, altura;
-    public string msgTrue, msgFalse;
-    [SerializeField] static public string contagem = "";
-    [SerializeField] public string contagemFrontEnd = "";
-    public int teste;
-    public string Nome01;
-    public string Nome02;
-    public string Nome03;
-    public string [] Nomes = new string [5];
-    public bool macaco;
-    public Color cor;
-    public Example Pessoa;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Nomes = new string[4];
-        /*print(Soma(10, 10));
-        print(Subtracao(15, 5));
-        print(Divisao(10,2));
-        print(Multiplicacao(5,2));
-        print("média" + Media(10, 10, 10));
-        ImprimirCrescente(1, 10);
-        ImprimirDecrescente(10,2);
-        tabuada(5);*/
-        //fatorial(20);
-        //ImparPar(true);
-        //contador(10);
-        //danoninho(2);
-        //yakult();
-        contagem = contagemFrontEnd;
-        exercicios.Contador(teste);
-
-        if (exercicios.validarIdade(idade, idadeMinima) == true)
-        {
-            print(msgTrue);
-        }
-        else
-        {
-            print(msgFalse);
-        }
-
-        exercicios.notas(n1, n2, n3);
-        exercicios.IMC(peso, altura);
-        exercicios.RaizQuadrada(81);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //5 EXERCICIOS
+    //TODOS A SAIDA DO PROGRAMA É PELO PRINT
+    //TODOS OS EXERCICIOS TEM QUE ESTAR DENTRO DESSE SCRIPTY
+    //TESTAR OS METODOS NA CLASSE LPOO 
+    //UTILIZAR O ARQUIIVO LEMBRETE.TXT PARA REVISÃO
+    //UTILIZAR RACIOCIONIO LOGIO PARA SABER QUANDO USAR AS FERRAMENTAS NO ARQUIVO TXT
+    //REOLVER OS EXERCICIOS USANDO SUA LOGIA DE PROGRAMAÇÃO
 
 
-    float Soma(float num1, float num2) 
-    {
-        return num1 + num2;
-    }
-    float Subtracao(float num1, float num2) 
-    {
-        return num1 - num2;
-    }
-    float Divisao(float num1, float num2)
-    {
-        return num1 / num2;
-    }
-    float Multiplicacao(float num1, float num2)
-    {
-        return num1 * num2;
-    }
-    float Media(float num1, float num2, float num3) 
-    {
-        float media;
-        media = (num1 + num2 + num3) / 3;
+    // 1- FAÇA UMA CLASSE CHAMADA CALCULADORA COM OS METODOS SOMA SUBTRAÇÃO DIVIÇÃO E MULTPLICAÇÃO
 
-        return media;
-    }
-    int ImprimirCrescente(int num1, int num2)
-    {
-        
-        for (int i = num1; i <= num2; i++) 
-        {
-            print(i);
-        }
-        return 0;
-    }
+    // 2- FAÇA UMA METODO NA CLASSE CALCULLADORA QUE SAIDA É TODAS AS TABUADAS
 
-    int ImprimirDecrescente(int num1, int num2)
-    {
-        for(int i = num1; i >= num2; i--)
-        {
-            print("decrescente" + i);
-        }
-        return 0;
-    }
+    // 3- FAÇA UMA CLASSE CHAMADA PESSOA COM OS SEGUINTES ATRIBUTOS, NOME, IDADE, PESO, ALTURA, GENERO
 
-    int tabuada(int num1)
-    {
-        int resultado;
-        for(int i = 1; i <= 10; i++ )
-        {
-            resultado = i * num1;
-            print(num1 + "X" + i + "=" + resultado);
-        }
-        return 0;
-    }
+    // 4- FAÇA UMA METODO NA CLASSE PESSOA PARA CALCULAR O IMC DE PESSOA 
 
-    int fatorial(int num1)
-    {
-        int resultado = 1;
-        int num2 = 0;
-        string imprimir;
+    // 5- FAÇA UMA METODO PARA SABER IDADE ATUAL DA PESSOA
 
-        imprimir = (num1 + "!" + "=");
-        for ( int i = 1; i < num1 ; i++)
-        {
-            resultado = resultado * (i + 1);
-            imprimir = imprimir + (i + "x");
-            num2 = i;
-        }
-        imprimir = imprimir + ((num2 + 1) + "=");
-        print(imprimir + resultado);
-        return 0;
-    }
+    // DESAFIO - FAÇA UMA FUÇÃO NA CLASSE CALCULADORA PARA CALCULAR O FATORIAL DE UM NUMERO
 
-    //Uma função chamda ImparPar que recebe  parametro do tipo booleano se for true, ele imprime do 0 ate 10 
-    //apenas os numeros imperes e se o parametro for false, imprimir apenas os pares.
-    void ImparPar(bool num1)
-    {
-        if (num1 == false)
-        {
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    print(i);
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i % 2 != 0)
-                {
-                    print(i);
-                }
-            }
-        }
-    }
 
-    void contador(int num1)
-    {
-        int resultado = 0;
 
-        for (int i = 0; i < num1; i++)
-        {
-            print(resultado + "+" + (i + 1) + "=" + (resultado + (i + 1)));
-            resultado = resultado + (i + 1);        
-        }
-
-        print("Resultado final" + "=" + resultado);
-    }
-
-    void danoninho(int num1)
-    {
-        int resultado = 0;
-
-        for(int i = 10; i > 0; i--)
-        {
-            resultado = num1 * i;
-            print(i + "x" + num1 + "=" + resultado);
-        }
-    }
-
-    void yakult()
-    {
-        int resultado = 0;
-
-        for (int i = 1; i <= 10; i++)
-        {
-            for(int j = 0; j <= 10; j++)
-            {
-                resultado = i * j;
-                print(i + "x" + j + "=" + resultado);
-            }
-        }
-    }
-}
-
-public class exercicios : MonoBehaviour
-{
-
-    static public bool validarIdade(int idade, int idadeCerta)
-    {
-        if (idade >= idadeCerta)
-        {
-            return true;
-        }
-        
-        else
-        {
-            return false;
-        }
-        
-    }
-
-    static public float notas(float n1, float n2, float n3)
-    {
-        float media;
-        media = (n1 + n2 + n3) / 3;
-
-        if (media >= 6)
-        {
-            print("Você mandou bem, aproveite as férias");
-        }
-        else
-        {
-            print("Estude mais no próximo ano, não foi dessa vez :(");
-        }
-        return 0;
-    }
-
-    static public float IMC(float peso, float altura)
-    {
-        float IMC;
-        IMC = (altura * altura) / peso;
-        print("Seu IMC é :" + IMC);
-        return 0;
-    }
-
-    static public void RaizQuadrada(int raiz)
-    {
-       for (int i = 0; i < raiz; i++)
-        {
-            if (raiz == (i * i))
-            {
-                print("A raiz quadrada de" + raiz + "=" + i);
-            }
-            else
-            {
-                print("Coloque uma raiz normal seu maluco !!!! #@!");
-            }
-        }
-    }
-
-    static public void Contador(int numero)
-    {
-        for(int i = 0;i < numero; i++)
-        {
-            LPOO.contagem = LPOO.contagem + "" + i; //transformando um int em string ( STRING = "" + OUTROS TIPOS )
-            
-        }
-        print(LPOO.contagem);
-    }
 }
 
